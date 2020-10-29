6c085568d4d3d2240b446992b984b0c14a48f600
„Martyna <martysiaxdxx@gamil.com>
2020-10-29 12:51:50 +0100

    interface book service

---
+++ Interfaces/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using Bookshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop.Interfaces
+{
+    public interface IBookService
+    {
+        IQueryable<Book> GetAllBooks();
+        Book GetBookById(int i);
+        Book AddBook(Book book);
+        Book DeleteBook(int id);
+        Book UpdateBook(int id);
+    }
+}
