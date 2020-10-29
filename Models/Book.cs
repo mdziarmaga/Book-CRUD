b0979cbfca54433fd99d82bd95624564b660707f
„Martyna <martysiaxdxx@gamil.com>
2020-10-29 12:47:12 +0100

    db connection

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshop.Models
+{
+    public class Book
+    {
+        [Key]
+        public int IdBook { get; set; }
+        [Required(ErrorMessage = "Enter the title")]
+        public string Tittle { get; set; }
+    }
+}
